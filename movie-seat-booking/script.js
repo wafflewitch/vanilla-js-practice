928429f13126717ebd346698dc263ad662c653ff
wafflewitch <jessicapruitt717@gmail.com>
2020-07-19 12:11:16 -0500

    In JS: get an array of the indexes of selected seats and preserve it in local storage; add a function to locally preserve the index and price of the currently selected movie, then call it in the movie select event listener.

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -7,9 +7,20 @@ let ticketPrice = +movieSelect.value; // the + sign converts it to a number
 
 // // // Functions // // //
 
+// Locally preserve index and price of selected movie
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Update count and total
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount;
@@ -21,6 +32,7 @@ function updateSelectedCount() {
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
