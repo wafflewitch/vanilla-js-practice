1c8c737e578e1eb61c328cc48dde9f3075ff7393
wafflewitch <jessicapruitt717@gmail.com>
2020-07-18 23:23:52 -0500

    Add functions for checkInputLength and checkInputMatch, and call them inside event listener.

--- form-validator/script.js
+++ form-validator/script.js
@@ -36,15 +36,36 @@ function getFieldName(input) {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
+// Check the length of a given input
+function checkInputLength(input, min, max) {
+  if (input.value.length < min) {
+    showError(
+      input,
+      `${getFieldName(input)} must be at least ${min} characters`
+    );
+  } else if (input.value.length > max) {
+    showError(
+      input,
+      `${getFieldName(input)} must be fewer than ${max} characters`
+    );
+  } else {
+    showSuccess(input);
+  }
+}
+
+// Ensure a match between two inputs
+function checkInputMatch(input1, input2) {
+  if (input1.value !== input2.value) {
+    showError(input2, 'Passwords must match');
+  }
+}
+
 // // // Event listeners // // //
 form.addEventListener('submit', function (e) {
   e.preventDefault();
 
   validateField([username, email, password, password2]);
-
-  // if (username.value === '') {
-  //   showError(username, 'Username is required');
-  // } else {
-  //   showSuccess(username);
-  // }
+  checkInputLength(username, 4, 20);
+  checkInputLength(password, 8, 25);
+  checkInputMatch(password, password2);
 });
