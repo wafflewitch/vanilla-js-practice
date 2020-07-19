52a1ab8956b1609984a831f9a26c842da656b06d
wafflewitch <jessicapruitt717@gmail.com>
2020-07-18 23:10:00 -0500

    Add a new function called getFieldName and call it in validateField function.

--- form-validator/script.js
+++ form-validator/script.js
@@ -24,11 +24,16 @@ function showSuccess(input) {
 function validateField(inputsArray) {
   inputsArray.forEach(function (input) {
     if (input.value.trim() === '') {
-      showError(input, `${input.id} is required`);
+      showError(input, `${getFieldName(input)} is required`);
     } else {
       showSuccess(input);
     }
   });
+}
+
+// Get the name of a given input field
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 // // // Event listeners // // //
