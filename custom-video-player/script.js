5a5cd9dffa6aef7a1604aa37d2576756144ffb80
wafflewitch <jessicapruitt717@gmail.com>
2020-07-20 09:56:53 -0500

    In JS: complete functions for updateProgressBar and setVideoProgress; add missing  double-quotes in updatePlayBtn function.

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -18,20 +18,34 @@ function toggleVideoStatus() {
 // Update play/pause button
 function updatePlayBtn() {
   if (video.paused) {
-    playBtn.innerHTML = '<i class="fa fa-play fa-2x></i>';
+    playBtn.innerHTML = '<i class="fa fa-play fa-2x"></i>';
   } else {
-    playBtn.innerHTML = '<i class="fa fa-pause fa-2x></i>';
+    playBtn.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
 
 // Update progress and timestamp
 function updateProgressBar() {
-  return true;
+  progressBar.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) {
+    minutes = '0' + String(minutes);
+  }
+
+  // Get seconds
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) {
+    seconds = '0' + String(seconds);
+  }
+
+  timestamp.innerHTML = `${minutes}:${seconds}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
 // Stop video
